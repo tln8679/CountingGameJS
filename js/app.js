55fba2395ace649e0b7a77fc348eb5d13e01ecdf
Taylor Noble <32083297+tln8679@users.noreply.github.com>
2018-05-08 18:46:15 -0400

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,11 @@
+/**
+ * @author Taylor Noble
+ */
+
+ var cardsLeft = document.getElementById("cardsLeft");
+ var yourChoices = document.getElementById("yourChoices");
+ var pcChoices = document.getElementById("pcChoices");
+
+ function onClick(clicked_id){
+     alert(clicked_id);
+ }
